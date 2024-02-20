a26c7188a0202ace1dbb82ff03a7948919ef8ab6
carovasq <42carovasq@gmail.com>
2024-02-20 13:50:28 +0100

    changes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,11 @@
-
+import Navbar from "../Navbar/Navbar"
 
 
 function App() {
 
   return (
     <>
-      
+      <Navbar />
     </>
   )
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <>
+      <h1>Soy un Navbar</h1>
+    </>
+  );
+}
+
+export default Navbar;
