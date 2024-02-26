454be1e9ac02010ace7f27b9e1053e45018a2778
Roger German Cecconello <nia.luna12@gmail.com>
2024-02-26 13:01:59 +0100

    modified footer code

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,62 +1,43 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
-  return (
-    <div className="container">
-      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-        <div className="col-md-4 d-flex align-items-center">
-          <a href="/" className="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
-            <svg className="bi" width="30" height="24"><use xlinkHref="#bootstrap"/></svg>
-          </a>
-          <span className="mb-3 mb-md-0 text-body-secondary">&copy; 2024 Júzcar Market, Inc</span>
-        </div>
+    return (
+        <footer style={footerStyle}>
+            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <p style={textStyle}>© 2024, Júzcar Market - Todos los derechos reservados</p>
+                <div>
+                    <a href="https://www.facebook.com/tucuenta" target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faFacebook} size="2x" style={iconStyle} />
+                    </a>
+                    <a href="https://twitter.com/tucuenta" target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faTwitter} size="2x" style={iconStyle} />
+                    </a>
+                    <a href="https://www.instagram.com/tucuenta" target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faInstagram} size="2x" style={iconStyle} />
+                    </a>
+                </div>
+            </div>
+    
+        </footer>
+    );
+};
 
-        <ul className="nav col-md-4 justify-content-end list-unstyled d-flex dark">
-          <li className="ms-3"><a className="text-body-secondary" href="#"><svg className="bi" width="24" height="24"><use xlinkHref="#twitter"/></svg></a></li>
-          <li className="ms-3"><a className="text-body-secondary" href="#"><svg className="bi" width="24" height="24"><use xlinkHref="#instagram"/></svg></a></li>
-          <li className="ms-3"><a className="text-body-secondary" href="#"><svg className="bi" width="24" height="24"><use xlinkHref="#facebook"/></svg></a></li>
-        </ul>
-      </footer>
+const footerStyle = {
+    backgroundColor: '#44BAD3',
+    color: '#fff',
+    textAlign: 'center',
+    padding: '1rem',
+};
 
+const iconStyle = {
+    marginRight: '1rem',
+    color: '#fff',
+};
 
-      <footer className="py-5">
-        <div className="row">
-          <div className="col-6 col-md-2 mb-3">
-            <h5>Sección</h5>
-            <ul className="nav flex-column">
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Únete</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Contacto</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Preguntas frecuentes</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Quiénes somos</a></li>
-            </ul>
-          </div>
-
-          <div className="col-md-5 offset-md-1 mb-3">
-            <form>
-              <h5>Suscribete a nuestras novedades diarias</h5>
-              <p>Pitufeando entre nuestras noticias.</p>
-              <div className="d-flex flex-column flex-sm-row w-100 gap-2">
-                <label htmlFor="newsletter1" className="visually-hidden">Email address</label>
-                <input id="newsletter1" type="text" className="form-control" placeholder="Email address" />
-                <button className="btn btn-primary" type="button">Subscribe</button>
-              </div>
-            </form>
-          </div>
-        </div>
-
-        <div className="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top bg-primary">
-          <ul className="list-unstyled d-flex">
-            <li className="ms-3"><a className="link-body-emphasis" href="#"><svg className="bi" width="24" height="24"><use xlinkHref="#twitter"/></svg></a></li>
-            <li className="ms-3"><a className="link-body-emphasis" href="#"><svg className="bi" width="24" height="24"><use xlinkHref="#instagram"/></svg></a></li>
-            <li className="ms-3"><a className="link-body-emphasis" href="#"><svg className="bi" width="24" height="24"><use xlinkHref="#facebook"/></svg></a></li>
-          </ul>
-        </div>
-      </footer>
-    </div>
-
-  );
-}
+const textStyle = {
+    margin: '0',
+};
 
 export default Footer;
