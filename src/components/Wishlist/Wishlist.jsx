27ad935debf5d4b6683f99802b5539269cf6c431
Carolay Vasquez <151535611+carovasq@users.noreply.github.com>
2024-02-29 14:57:28 +0100

    add productmodal.jsx

--- src/components/Wishlist/Wishlist.jsx
+++ src/components/Wishlist/Wishlist.jsx
@@ -1,10 +1,20 @@
-import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
-import './Wishlist.css';
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import ProductModal from "../ProductModal/ProductModal";
+import "./Wishlist.css";
 
 // Componente de Producto
-function ProductCard({ imageUrl, name, price, addToFavorites, removeFromFavorites, isLiked }) {
+function ProductCard({
+  imageUrl,
+  name,
+  price,
+  description,
+  addToFavorites,
+  removeFromFavorites,
+  isLiked,
+  onProductSelect,
+}) {
   const handleLikeClick = () => {
     if (isLiked) {
       removeFromFavorites(name);
@@ -14,65 +24,100 @@ function ProductCard({ imageUrl, name, price, addToFavorites, removeFromFavorite
   };
 
   return (
-    <div className="product-card">
+    <div
+      className="product-card"
+      onClick={() => onProductSelect({ imageUrl, name, price, description })}
+    >
       <img src={imageUrl} alt="Producto" className="product-image" />
       <h5>{name}</h5>
       <p>{price}</p>
       <button className="heart-button" onClick={handleLikeClick}>
-        <FontAwesomeIcon icon={faHeart} style={{ color: isLiked ? '#FF635E' : '#44BAD3' }} />
+        <FontAwesomeIcon
+          icon={faHeart}
+          style={{ color: isLiked ? "#FF635E" : "#44BAD3" }}
+        />
       </button>
-      <FontAwesomeIcon icon={faShoppingCart} style={{ marginLeft: '10px', color: '#44BAD3' }} />
+      <FontAwesomeIcon
+        icon={faShoppingCart}
+        style={{ marginLeft: "10px", color: "#44BAD3" }}
+      />
     </div>
   );
 }
 
 function Wishlist() {
   const [favorites, setFavorites] = useState([]);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [showModal, setShowModal] = useState(false);
 
   const addToFavorites = (product) => {
     setFavorites([...favorites, product]);
   };
 
   const removeFromFavorites = (productName) => {
-    const updatedFavorites = favorites.filter(item => item.name !== productName);
+    const updatedFavorites = favorites.filter(
+      (item) => item.name !== productName
+    );
     setFavorites(updatedFavorites);
   };
+
+  const handleProductSelect = (product) => {
+    setSelectedProduct(product);
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
   return (
     <>
       <h1>Productos más vendidos este mes</h1>
       <div className="best-sellers">
-        {/* Renderizar instancias separadas de ProductCard para cada producto */}
         <ProductCard
           imageUrl="https://i.postimg.cc/TPmwVpvy/bodybeb.jpg"
           name="Body bebé pitufo"
           price="20,00€"
+          description="Soy la descripcion 1"
           addToFavorites={addToFavorites}
           removeFromFavorites={removeFromFavorites}
-          isLiked={favorites.some(item => item.name === "Body bebé pitufo")}
+          isLiked={favorites.some((item) => item.name === "Body bebé pitufo")}
+          onProductSelect={handleProductSelect}
         />
         <ProductCard
           imageUrl="https://i.postimg.cc/JnJt3xSC/peluchepitufo.jpg"
           name="Peluche pitufo"
           price="30,00€"
+          description="Soy la descripcion 2"
           addToFavorites={addToFavorites}
           removeFromFavorites={removeFromFavorites}
-          isLiked={favorites.some(item => item.name === "Peluche pitufo")}
+          isLiked={favorites.some((item) => item.name === "Peluche pitufo")}
+          onProductSelect={handleProductSelect}
         />
         <ProductCard
           imageUrl="https://i.postimg.cc/fTLRHHhQ/figurapitufina.jpg"
           name="Figura pitufina"
           price="15,00€"
+          description="Soy la descripcion 3"
           addToFavorites={addToFavorites}
           removeFromFavorites={removeFromFavorites}
-          isLiked={favorites.some(item => item.name === "Figura pitufina")}
+          isLiked={favorites.some((item) => item.name === "Figura pitufina")}
+          onProductSelect={handleProductSelect}
         />
       </div>
+      {showModal && selectedProduct && (
+        <ProductModal product={selectedProduct} closeModal={closeModal} />
+      )}
       <div>
         <h1>Mis Favoritos</h1>
         <ul>
           {favorites.map((item, index) => (
             <li key={index}>
-              <img src={item.imageUrl} alt={item.name} style={{ width: '50px' }} />
+              <img
+                src={item.imageUrl}
+                alt={item.name}
+                style={{ width: "50px" }}
+              />
               <p>{item.name}</p>
               <p>{item.price}</p>
             </li>
@@ -80,7 +125,7 @@ function Wishlist() {
         </ul>
       </div>
     </>
-  )
+  );
 }
 
-export default Wishlist
+export default Wishlist;
