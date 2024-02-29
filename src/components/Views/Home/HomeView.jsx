896f5b3b848f32bbf4593fc01637825b0ff63711
Roger German Cecconello <nia.luna12@gmail.com>
2024-02-29 10:50:24 +0100

    merged with Lidia for the second time

--- src/components/Views/Home/HomeView.jsx
+++ src/components/Views/Home/HomeView.jsx
@@ -10,7 +10,8 @@ const HomeView = () => {
       <CardsForm/>
        <img src="https://i.postimg.cc/nrxdhNkC/alcalde-1-1.png" alt="" />
       <Wishlist/>
-    <div className='home-view'>
+   
+        <div className='home-view'>
         <img className='home-view-photo' src="https://i.postimg.cc/Ss5SPGVD/pisajej-zcar-1-1.png" alt="" />
 
         <h1 className='home-view-title'>Colaboradores</h1>
@@ -18,9 +19,9 @@ const HomeView = () => {
          <section className='colab-section-img'>
          <img  className='pitufo-dad' src="https://i.postimg.cc/DfsmLqRm/pngwing-com-2.png" alt="" />
           <h2 className='colab-text'>En el pintoresco pueblo de Júzcar, los vecinos tejen historias con sus manos. Antonio Luis Jiménez guía visitas, María Luisa del Rosario crea camisetas y mochilas, y Encarnación Soledad Robles da vida a juguetes. Estos vecinos, con sus manos curtidas y corazones llenos de pasión, han tejido una red de creatividad que se extiende más allá de las montañas. Sus obras, ahora digitalizadas en una web, nos invitan a explorar la esencia de Júzcar: la unión entre lo ancestral y lo contemporáneo.</h2>
-          </section>
-      <Carousel1/> 
+          </section> 
       </div>
+
       <h1 className='home-view-title'>Nuestros productos</h1>
       <div className="product-card">
         <img src={imageUrl} alt="Producto" className="product-image" />
