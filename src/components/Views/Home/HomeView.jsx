4e3c92f8fc257d650dd3654939db1acb01b91b7e
EstyGlez <estibalizzmakeup@gmail.com>
2024-02-29 12:09:28 +0100

    filtrado categorías

--- src/components/Views/Home/HomeView.jsx
+++ src/components/Views/Home/HomeView.jsx
@@ -1,11 +1,11 @@
-import Carousel1 from "../../Carousel/Carousel";
+
 import CardsForm from '../../cardsform/CardsForm.jsx';
 
 const HomeView = () => {
   return (
     <div>
 
-      <Carousel1/>
+      
       <CardsForm/>
        <img src="https://i.postimg.cc/nrxdhNkC/alcalde-1-1.png" alt="" />
     </div>
