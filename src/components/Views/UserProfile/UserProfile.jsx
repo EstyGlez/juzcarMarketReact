87cd5b411d95ab5c9e277d8c8e01b79c7a2c6f33
EstyGlez <estibalizzmakeup@gmail.com>
2024-03-05 13:12:01 +0100

    changes

--- src/components/Views/UserProfile/UserProfile.jsx
+++ src/components/Views/UserProfile/UserProfile.jsx
@@ -2,24 +2,22 @@ import React from 'react';
 import CardsForm from '../../cardsform/CardsForm.jsx';
 
 const UserProfile = () => {
-  // Recuperar el nombre de usuario del almacenamiento local
   const username = localStorage.getItem('username');
 
-  // Función para manejar el cierre de sesión
+  
   const handleLogout = () => {
-    // Eliminar el nombre de usuario del almacenamiento local
+   
     localStorage.removeItem('username');
-    // Redirigir al usuario a la página de inicio de sesión o a donde prefieras
-    // Aquí podrías utilizar react-router para la navegación
+    
   };
 
   return (
     <div>
-      {/* Mostrar un mensaje de bienvenida personalizado */}
+      
       <h2>Bienvenid@ {username} a tu perfil de usuario</h2>
-      {/* Agregar un botón de salida */}
+      
       <button onClick={handleLogout}>Cerrar sesión</button>
-      {/* Agregar el componente CardsForm */}
+      
       <CardsForm />
     </div>
   );
